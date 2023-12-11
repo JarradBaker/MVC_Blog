bf3efba69528c9403760c948b00070537dd8f6c5
JarradBaker <jarradbaker@outlook.com>
2023-12-11 08:18:02 +0000

    testing GitHub commits through Viscual Studio

--- Blog/Blog.Web/Models/Domain/BlogPost.cs
+++ Blog/Blog.Web/Models/Domain/BlogPost.cs
@@ -14,7 +14,7 @@ namespace Blog.Web.Models.Domain
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool Visible { get; set; }
-
+        // Testing
         public ICollection<Tag> Tags { get; set; }
 
 
